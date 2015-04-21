2e3bf365f95de2b71cf8b23a4285ac6b3e69e718
lexiconby <lexiconby@hotmail.com>
2015-04-21 01:37:16 -0700

    added some code
    solve doesn’t work…even on the command line

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,46 @@
 ## R Programming assignment 2
 ## Written by Billy Yeh
 
-
-## this function makes a cache Matrix
-makeCacheMatrix <- function(x = matrix()) {
-    #testing github
+## This function creates the cached matrix
+makeCacheMatrix <- function( theMatrix = matrix()) {
+    #initialize the inverted matrix we plan on caching to null
+    theMatrixInverted <- NULL
+    
+    #setter of the matrix
+    setTheMatrix <- function(theMatrixLocal) {
+        theMatrix <<- theMatrixLocal
+        #when the matrix changes, the cache will be flushed
+        theMatrixInverted <<- NULL
+    }
+    #getter of the matrix simply returns the matrix
+    getTheMatrix <- function() theMatrix
+    #setter of inverted matrix is when caching is created
+    setTheMatrixInverted <- function(solve) theMatrix
+    #getter of inverted matrix simply returns the inverted matrix
+    getTheMatrixInverted <- function() theMatrixInverted
+    
+    list (
+        setTheMatrix = setTheMatrix, 
+        getTheMatrix = getTheMatrix,
+        setTheMatrixInverted = setTheMatrixInverted,
+        getTheMatrixInverted = getTheMatrixInverted
+    )
 }
 
 
-## this function will return the inverse of a matrix passed in
-## it will use the cached matrix for optimization
-
+## This function is the main entry point to invert the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    theMatrix <- x$getTheMatrix()
+    
+    theMatrixInverted <- x$getTheMatrixInverted()
+    
+    if(!is.null(theMatrixInverted)) {
+        message("getting cached inverted matrix")
+        return(theMatrixInverted)
+    } else {
+        NULL
+    }
+    
+    
 }
