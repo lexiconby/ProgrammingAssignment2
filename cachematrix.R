b6a62e14f8139f7aa6e5adbcdbae02c110102b3f
lexiconby <lexiconby@hotmail.com>
2015-04-20 18:18:34 -0700

    initial checking to test
    checking in the shell and testing out github wiring job

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming assignment 2
+## Written by Billy Yeh
 
-## Write a short comment describing this function
 
+## this function makes a cache Matrix
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## this function will return the inverse of a matrix passed in
+## it will use the cached matrix for optimization
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
